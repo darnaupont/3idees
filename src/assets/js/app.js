088ee08fb31e61e8982083363682d35df87612d3
Damià Arnau Pont <damiarnau@gmail.com>
2017-03-31 23:20:10 +0200

    Arreglant error varis post-caos, actualització imatges

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,37 +1,23 @@
 $(document).foundation();
-
-
 $(document).ready(function() {
-
-            $('#fullpage').fullpage({
-                anchors: ['3idees', 'objetivos', 'ganarmas', 'rentabilidad', 'publicidad', 'trabajo', 'quieresmejorar','footer'],
-                navigation: true,
-                navigationPosition: 'right',
-                navigationTooltips: ['3idees', 'cumplir objetivos', 'ganar más', 'rentabilidad', 'publicidad', 'menos reuniones, más trabajo', '¿quieres mejorar?' ],
-                scrollingSpeed: 900,
-                            responsiveWidth: 1100,
-        //easing: 'easeInElastic',
-        //easingcss3: 'ease',
-        //verticalCentered: true,
-        //resize: false,
-        
-        //Custom selectors
-        //sectionSelector: '.section',
-        //slideSelector: '.slide',
+    $('#fullpage').fullpage({
+        anchors: ['3idees', 'ganarmas', 'resultadosreales','crecerrapido', 'quieresmejorar','footer'],
+        navigation: true,
+        navigationPosition: 'right',
+        scrollingSpeed: 900,
+        responsiveWidth: 1100,
         scrollOverflow: false,
         onLeave: function(index, nextIndex, direction){
         //leaving 1st section
-        if((nextIndex == 4)||(nextIndex == 6)){
+        if((nextIndex == 2)||(nextIndex == 4)){
            $('.arrow-black').addClass('white');
            $('#fp-nav ul li, .fp-slidesNav ul li').addClass('dotwhite');
            $('#fp-nav ul li .fp-tooltip.right').removeClass('textblack');
            $('.top-bar-left a').addClass('logo3ideeswhite');
            $('.top-bar-right ul li a').addClass('textwhite');
-
         }
-
         //back to the 1st section
-        if((index == 4)||(index == 6)){
+        if((index == 2)||(index == 4)){
             $('.arrow-black').removeClass('white');
             $('#fp-nav ul li, .fp-slidesNav ul li').removeClass('dotwhite');
             $('#fp-nav ul li .fp-tooltip.right').addClass('textblack');
@@ -39,28 +25,20 @@ $(document).ready(function() {
             $('.top-bar-right ul li a').removeClass('textwhite');
         }
         if(index == 2){
-            $('.top-bar .gradient-border .text').removeClass('objectivesbg');
-        }
-        //back to the 1st section
-        if(nextIndex == 2){
-            $('.top-bar .gradient-border .text').addClass('objectivesbg');
-
-        }
-                if(index == 4){
             $('.top-bar .gradient-border .text').removeClass('goalsbg');
         }
         //back to the 1st section
-        if(nextIndex == 4){
+        if(nextIndex == 2){
             $('.top-bar .gradient-border .text').addClass('goalsbg');
         }
-                if(index == 6){
+        if(index == 4){
             $('.top-bar .gradient-border .text').removeClass('moreworkbg');
         }
         //back to the 1st section
-        if(nextIndex == 6){
+        if(nextIndex == 4){
             $('.top-bar  .gradient-border .text').addClass('moreworkbg');
         }
-    }      
+    }
             });
         });
 
